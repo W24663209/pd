b0ee25b0a0ee650a2615f77e9e54b490f6c0ce50
mac01 <mac01@gmail.com>
2024-05-07 17:53:42 +0800

    1

--- src/utils/parse_util.py
+++ src/utils/parse_util.py
@@ -1,5 +1,16 @@
+import json
+
 import openpyxl
+import requests
 from utils import mysql_util
+import concurrent.futures
+
+
+def save(item):
+    url = 'https://webhook.kingpay.io/api/providusbank/payin'
+    headers = {"Content-Type": "application/json"}
+    response = requests.post(url, data=json.dumps(item), headers=headers)
+    print(response.text)
 
 
 def xlsx(path):
@@ -14,15 +25,26 @@ def xlsx(path):
     index = 0
     rows = sheet.iter_rows(values_only=True)
     max_row = rows.gi_frame.f_locals['max_row']
+    thread_pool = concurrent.futures.ThreadPoolExecutor(max_workers=10)
+    # for text in open('src/download/STATEMENT.csv').read().split('\n')[19:-5]:
+    #     pass
     for row in rows:
         if index >= 17 and max_row - 4 >= index:
-            mysql_util.update(f'''
-            insert into tbl_providus_bank (post_date, actual_transaction_date, narration, value_date, debit, credit,
-                               current_balance, dr_cr, doc_num) values ('{"','".join(row)}')
-            ''')
+            item = {}
+            item['postDate'] = row[0]
+            item['actualTransactionDate'] = row[1]
+            item['narration'] = row[2].replace('INWARD TRANSFER', '').replace('FROM', '')
+            item['valueDate'] = row[3]
+            item['debit'] = row[4]
+            item['credit'] = row[5]
+            item['currentBalance'] = row[6]
+            item['drCr'] = row[7]
+            item['docNum'] = row[8]
+            thread_pool.submit(save, item)
         index += 1
     # 关闭Excel文件
     workbook.close()
+    thread_pool.shutdown(wait=True)
 
 
 if __name__ == '__main__':
